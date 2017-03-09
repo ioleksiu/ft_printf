54335d96407e0e0e31345aedadfd6a82ea277e15
ioleksiu <ilia041@gmail.com>
2017-03-09 16:27:12 +0200

    %u uncommented

--- pf_cast.c
+++ pf_cast.c
@@ -78,7 +78,6 @@ void f_O(t_pf *a, va_list ap)
 
 void f_u(t_pf *a, va_list ap)
 {
-    /*
     uintmax_t i = cast_du(a,ap);
     int n = count_unum(i);
     int c_s = 0;
@@ -110,7 +109,6 @@ void f_u(t_pf *a, va_list ap)
         put_nchar('0', c_z, a);
         ft_put_unbr(i, a);
     }
-     */
 }
 
 void f_U(t_pf *a, va_list ap)
